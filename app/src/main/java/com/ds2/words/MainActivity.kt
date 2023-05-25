1cd7f771d04944848d04d21f31cb446da331e2a5
Etec Profº Horácio Augusto da Silveira <caioquir@gmail.com>
2023-05-24 22:36:33 -0300

    Projeto da Aula Completo

---
+++ app/src/main/java/com/ds2/words/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ds2.words
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ds2.words.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        recyclerView = binding.recyclerView
+// Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+}
